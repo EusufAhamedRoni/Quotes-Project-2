5cca7ec1d37f058aef7f080f86365894e07e4758
Eusuf Ahamed <eusufahmed789@gmail.com>
2020-08-19 11:55:47 +0600

    First Commit

---
+++ quotes/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import ListView, TemplateView
+from quotes.models import QuoteCategory, Quote
+
+class HomeView(ListView):
+    template_name = "home.html"
+    model = Quote
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        return query_set.select_related('quote_category')
